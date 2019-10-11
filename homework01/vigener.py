24f586275ce0e996e9322ce5b3861d0d459a91dd
olticher <55976490+olticher@users.noreply.github.com>
2019-10-12 00:35:05 +0300

    Изменила код

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -9,9 +9,8 @@ def encrypt_vigenere(plaintext, keyword):
     """
     # PUT YOUR CODE HERE
     ciphertext = ""
-    symbols = '1234567890.,-=<>?|+_)(*&^%$#@!'
     for j in plaintext:
-        if j in symbols:
+        if not j.isalpha():
             ciphertext += j
     if len(plaintext) > len(keyword):
         keyword += keyword*((len(plaintext) // len(keyword))-1)
@@ -23,9 +22,9 @@ def encrypt_vigenere(plaintext, keyword):
         else:
             value = ord(keyword[number])-97
         if char.isupper():
-            ciphertext += chr((ord(char) + value - 65) % 26 + 65)
+            ciphertext += chr((ord(char) + value - ord('A')) % 26 + ord('A'))
         else:
-            ciphertext += chr((ord(char) + value - 97) % 26 + 97)
+            ciphertext += chr((ord(char) + value - ord('a')) % 26 + ord('a'))
         number += 1
     return ciphertext
 
@@ -40,24 +39,23 @@ def decrypt_vigenere(ciphertext, keyword):
     'ATTACKATDAWN'
     """
     # PUT YOUR CODE HERE
-    symbols = '1234567890.,-=<>?|+_)(*&^%$#@!'
     plaintext = ''
     for j in ciphertext:
-        if j in symbols:
+        if not j.isalpha():
             plaintext += j
     if len(ciphertext) > len(keyword):
-        keyword += keyword*((len(ciphertext) // len(keyword))-1)
+        keyword += keyword*((len(ciphertext) // len(keyword)) - 1)
         keyword += keyword[:(len(ciphertext) % len(keyword))]
     number = 0
     for char in ciphertext:
         if keyword[number].isupper():
             value = ord(keyword[number]) - 65
         elif keyword[number].islower():
-            value = ord(keyword[number]) - 97 
+            value = ord(keyword[number]) - 97
         if char.isupper():
-            plaintext += chr((ord(char) - value - 65) % 26 + 65)
+            plaintext += chr((ord(char) - value - ord('A')) % 26 + ord('A'))
         elif char.islower():
-            plaintext += chr((ord(char) - value - 97) % 26 + 97)
-        number += 1  
+            plaintext += chr((ord(char) - value - ord('a')) % 26 + ord('a'))
+        number += 1
 
     return plaintext
