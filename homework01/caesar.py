15cad55ab2a2065b95500807d5c83c2ba161d237
olticher <plotskaja2001@yandex.ru>
2019-10-12 00:06:38 +0300

    Код исправлен

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -23,26 +23,26 @@ def encrypt_caesar(plaintext):
     return ciphertext
 
 
-def decrypt_caesar(ciphertext): 
-    """ 
-    Decrypts a ciphertext using a Caesar cipher. 
-    »> decrypt_caesar("SBWKRQ") 
-    'PYTHON' 
-    »> decrypt_caesar("sbwkrq") 
-    'python' 
-    »> decrypt_caesar("Sbwkrq3.6") 
-    'Python3.6' 
-    »> decrypt_caesar("") 
-    '' 
-    """ 
-    # PUT YOUR CODE HERE 
-    plaintext = '' 
-    symbols = '1234567890.,/!@#$%^&*()_-+={[] ' 
-    for char in ciphertext: 
+def decrypt_caesar(ciphertext):
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    »> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    »> decrypt_caesar("sbwkrq")
+    'python'
+    »> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    »> decrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    plaintext = ''
+    symbols = '1234567890.,/!@#$%^&*()_-+={[] '
+    for char in ciphertext:
         if char.islower():
             plaintext += chr((ord(char) - 3 - 'a') % 26 + 'a')
-        elif char.isupper(): 
-            plaintext += chr((ord(char) - 3 - 'A') % 26 + 'A') 
+        elif char.isupper():
+            plaintext += chr((ord(char) - 3 - 'A') % 26 + 'A')
         elif not (char.islower() or char.isupper()):
             plaintext += char
 
